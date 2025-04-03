05eb5e098cb905931b19fd744da76d2aca739516
estevao-santos-ribeiro <estevaosantos@uniplaclages.edu.br>
2025-04-03 11:37:48 -0300

    Implementada as movimentações na conta

--- ContaCorrente.ConsoleApp/Program.cs
+++ ContaCorrente.ConsoleApp/Program.cs
@@ -4,7 +4,57 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int numeroIdentificacao;
+            decimal saldoDisponivel, limiteDebito, valorTransacao;
+            char opcaoTransacao;
+            string tipoTransacao;
+            string[] movimentacoes = new string[100];
+
+            // menu
+            Console.WriteLine("--------------------------------------------------------------");
+            Console.WriteLine("                      Conta Corrente");
+            Console.WriteLine("--------------------------------------------------------------");
+            Console.WriteLine("Para começar digite o número de identificação da sua conta!");
+            Console.Write("Número da conta: ");
+            numeroIdentificacao = Convert.ToInt32(Console.ReadLine());
+
+            while (true)
+            {
+                Console.WriteLine("Escolha o tipo de transação que deseja realizar.");
+                Console.WriteLine("1 - Depósito");
+                Console.WriteLine("2 - Saque");
+                Console.WriteLine("3 - Transferência");
+                Console.Write("Digite a sua opção: ");
+                opcaoTransacao = Convert.ToChar(Console.ReadLine()!);
+                if(opcaoTransacao != '1' && opcaoTransacao != '2' && opcaoTransacao != '3')
+                {
+                    Console.WriteLine("Opção inválida, tente novamente.");
+                } else
+                {
+                    break;
+                }
+            }
+
+            if (opcaoTransacao == '1')
+            {
+                tipoTransacao = "depósito";
+            }
+            else if (opcaoTransacao == '2')
+            {
+                tipoTransacao = "saque";
+            }
+            else
+            {
+                tipoTransacao = "transferência";
+            }
+
+            Console.Write("Digite o valor da transação: ");
+            valorTransacao = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine($"A conta {numeroIdentificacao} realizou a operação de {tipoTransacao} de R$ {valorTransacao}");
+
+            Console.ReadLine();
         }
     }
 }
