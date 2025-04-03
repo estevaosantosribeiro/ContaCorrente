494fa2c097459c4423520f4069dc3b7598a4a625
estevao-santos-ribeiro <estevaosantos@uniplaclages.edu.br>
2025-04-03 12:25:39 -0300

    Implementadas ações de menu

--- ContaCorrente.ConsoleApp/Program.cs
+++ ContaCorrente.ConsoleApp/Program.cs
@@ -4,57 +4,116 @@
     {
         static void Main(string[] args)
         {
-            int numeroIdentificacao;
-            decimal saldoDisponivel, limiteDebito, valorTransacao;
-            char opcaoTransacao;
+            int indiceMovimentacao = 0, numeroIdentificacao;
+            decimal saldoDisponivel, valorAdicional, limitePermitido, valorTransacao;
+            char acaoEscolhida;
             string tipoTransacao;
-            string[] movimentacoes = new string[100];
+            string[] registroMovimentacoes = new string[100];
 
             // menu
-            Console.WriteLine("--------------------------------------------------------------");
-            Console.WriteLine("                      Conta Corrente");
-            Console.WriteLine("--------------------------------------------------------------");
-            Console.WriteLine("Para começar digite o número de identificação da sua conta!");
+            ExibirCabecalho();
             Console.Write("Número da conta: ");
             numeroIdentificacao = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Saldo disponível: ");
+            saldoDisponivel = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("Valor adicional: ");
+            valorAdicional = Convert.ToDecimal(Console.ReadLine());
+
+            limitePermitido = saldoDisponivel + valorAdicional;
 
             while (true)
             {
-                Console.WriteLine("Escolha o tipo de transação que deseja realizar.");
-                Console.WriteLine("1 - Depósito");
-                Console.WriteLine("2 - Saque");
-                Console.WriteLine("3 - Transferência");
+                Console.Clear();
+                ExibirMenu();
                 Console.Write("Digite a sua opção: ");
-                opcaoTransacao = Convert.ToChar(Console.ReadLine()!);
-                if(opcaoTransacao != '1' && opcaoTransacao != '2' && opcaoTransacao != '3')
+                acaoEscolhida = Convert.ToChar(Console.ReadLine()!);
+
+                if (acaoEscolhida == '1')
                 {
-                    Console.WriteLine("Opção inválida, tente novamente.");
-                } else
+                    Console.Write("Digite o valor da transação: ");
+                    valorTransacao = Convert.ToDecimal(Console.ReadLine());
+                    if (valorTransacao < limitePermitido)
+                    {
+                        saldoDisponivel -= valorTransacao;
+                        tipoTransacao = "saque";
+                        registroMovimentacoes[indiceMovimentacao] = $"A conta {numeroIdentificacao} realizou a operação de {tipoTransacao} de R$ {valorTransacao}";
+                        indiceMovimentacao++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Limite permitido excedido! Tente novamente.");
+                    }
+                }
+                else if (acaoEscolhida == '2')
+                {
+                    Console.Write("Digite o valor da transação: ");
+                    valorTransacao = Convert.ToDecimal(Console.ReadLine());
+                    saldoDisponivel += valorTransacao;
+                    tipoTransacao = "depósito";
+                    registroMovimentacoes[indiceMovimentacao] = $"A conta {numeroIdentificacao} realizou a operação de {tipoTransacao} de R$ {valorTransacao}";
+                    indiceMovimentacao++;
+                }
+                else if (acaoEscolhida == '3')
+                {
+                    Console.WriteLine($"Saldo disponível: R$ {saldoDisponivel}");
+                }
+                else if (acaoEscolhida == '4')
+                {
+                    if (registroMovimentacoes.Length > 0)
+                    {
+                        for (int i = 0; i < indiceMovimentacao; i++)
+                        {
+                            Console.WriteLine(registroMovimentacoes[i]);
+                        }
+                    } else
+                    {
+                        Console.WriteLine("Ainda não há movimentações!");
+                    }
+                }
+                else if(acaoEscolhida == '5')
+                {
+                    Console.Write("Digite o valor da transação: ");
+                    valorTransacao = Convert.ToDecimal(Console.ReadLine());
+                    if (valorTransacao < limitePermitido)
+                    {
+                        saldoDisponivel -= valorTransacao;
+                        tipoTransacao = "transferência";
+                        registroMovimentacoes[indiceMovimentacao] = $"A conta {numeroIdentificacao} realizou a operação de {tipoTransacao} de R$ {valorTransacao}";
+                        indiceMovimentacao++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Limite permitido excedido! Tente novamente.");
+                    }
+                }
+                else
                 {
                     break;
                 }
+                Console.ReadLine();
             }
-
-            if (opcaoTransacao == '1')
-            {
-                tipoTransacao = "depósito";
-            }
-            else if (opcaoTransacao == '2')
-            {
-                tipoTransacao = "saque";
-            }
-            else
-            {
-                tipoTransacao = "transferência";
-            }
-
-            Console.Write("Digite o valor da transação: ");
-            valorTransacao = Convert.ToDecimal(Console.ReadLine());
-
-            Console.WriteLine();
-            Console.WriteLine($"A conta {numeroIdentificacao} realizou a operação de {tipoTransacao} de R$ {valorTransacao}");
 
             Console.ReadLine();
+        }
+
+        static void ExibirCabecalho()
+        {
+            Console.WriteLine("--------------------------------------------------------------");
+            Console.WriteLine("                      Conta Corrente");
+            Console.WriteLine("--------------------------------------------------------------");
+            Console.WriteLine("Para começar digite o número de identificação, o saldo e o \nvalor adicional da sua conta.");
+        }
+
+        static void ExibirMenu()
+        {
+            Console.WriteLine();
+            Console.WriteLine("1 - Saque");
+            Console.WriteLine("2 - Depósito");
+            Console.WriteLine("3 - Consuta de saldo");
+            Console.WriteLine("4 - Emissão de extrato");
+            Console.WriteLine("5 - Transferência entre contas");
+            Console.WriteLine("6 - Sair");
+            Console.WriteLine();
         }
     }
 }
